8f6947b6b3319f3ea2f9675d5b8172a9e39f39eb
Student <ziwei.polaris@gmail.com>
2019-08-12 17:52:24 +0100

    Updates

---
+++ precache-manifest.e5f1a0d760b342de87fa44ad66d22608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d438577ea10a4c629e98f35ae4e4ac",
+    "url": "/squiggler/index.html"
+  },
+  {
+    "revision": "82945b9f6943c3c33f74",
+    "url": "/squiggler/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "035607f0a4d5032fd947",
+    "url": "/squiggler/static/js/2.b0966223.chunk.js"
+  },
+  {
+    "revision": "82945b9f6943c3c33f74",
+    "url": "/squiggler/static/js/main.251b8117.chunk.js"
+  },
+  {
+    "revision": "af0c5721f249fa48fc0c",
+    "url": "/squiggler/static/js/runtime~main.3c083e4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/squiggler/static/media/logo.5d5d9eef.svg"
+  }
+]);
